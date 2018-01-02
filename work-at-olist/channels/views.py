da1da8605eb8a5ff80d82f073ca9bf0f111f43a7
Otavio Braga <otaviobraga@MacBook-Air-de-Otavio.local>
2018-01-02 21:12:15 -0200

    ORM optmizations

--- work-at-olist/channels/views.py
+++ work-at-olist/channels/views.py
@@ -4,22 +4,22 @@ from rest_framework import generics
 
 
 class CategoriesListView(generics.ListAPIView):
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().select_related('parent', 'channel')
     serializer_class = CategorySerializer
 
 
 class ChannelsListView(generics.ListAPIView):
-    queryset = Channel.objects.all()
+    queryset = Channel.objects.all().prefetch_related('category_set')
     serializer_class = ChannelSerializer
 
 
 class ChannelDetailView(generics.RetrieveAPIView):
-    queryset = Channel.objects.all()
+    queryset = Channel.objects.all().prefetch_related('category_set')
     serializer_class = ChannelSerializer
     lookup_field = 'uuid'
 
 
 class CategoryDetailView(generics.RetrieveAPIView):
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().select_related('parent', 'channel')
     serializer_class = CategorySerializer
     lookup_field = 'uuid'
