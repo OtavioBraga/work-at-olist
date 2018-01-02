61989014094480d3ea96f99100e371ca35332ec3
Otavio Braga <otaviobraga@MacBook-Air-de-Otavio.local>
2018-01-02 18:35:26 -0200

    Changed urls to use django 2 url schema

--- work-at-olist/channels/urls.py
+++ work-at-olist/channels/urls.py
@@ -1,15 +1,16 @@
 from django.contrib.auth import views as auth_views
-from channels.views import CategoriesList, ChannelsList
-from django.conf.urls import url
+from channels.views import CategoriesList, ChannelsList, ChannelDetail
+from django.urls import path
+
+
+app_name = "channels"
 
 urlpatterns = [
 
-    url(r'^category/',
-        CategoriesList.as_view(),
-        name='categories-list'),
+    path('category/', CategoriesList.as_view(), name='categories-list'),
 
-    url(r'^channel/',
-        ChannelsList.as_view(),
-        name='channels-list')
+    path('channel/', ChannelsList.as_view(), name='channels-list'),
+
+    path('channel/<uuid:uuid>/', ChannelDetail.as_view(), name='channel-detail')
 
 ]
