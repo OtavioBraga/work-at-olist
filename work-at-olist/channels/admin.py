6cddc023b3001c3313cfab58bd697d3dd124323a
Otavio Braga <otaviobraga@MacBook-Air-de-Otavio.local>
2018-01-02 14:50:41 -0200

    Added the models to represent a channel and his categories

--- work-at-olist/channels/admin.py
+++ work-at-olist/channels/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from channels.models import Channel, Category
 
-# Register your models here.
+admin.site.register(Channel)
+admin.site.register(Category)
--- work-at-olist/channels/models.py
+++ work-at-olist/channels/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Category(models.Model):
+    '''
+        This model represent a category on databse.
+        Categories can be any general or comprehensive division.
+        E.g: Games > XBOX > Adventure...
+    '''
+
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True
+    )
+
+    name = models.CharField(
+        max_length=255,
+    )
+
+    channel = models.ForeignKey(
+        'Channel',
+        on_delete=models.CASCADE
+    )
+
+    parent = models.ForeignKey(
+        'Category',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ('created_at', )
+
+    def __str__(self):
+        return self.name
+
+
+class Channel(models.Model):
+    '''
+        This model represent a channel on databse.
+        A channel is a way of making a product available.
+        E.g: Submarino marketplace, Americanas marketplace...
+    '''
+
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True
+    )
+
+    name = models.CharField(
+        max_length=255,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Channel'
+        verbose_name_plural = 'Channels'
+        ordering = ('created_at', )
+
+    def __str__(self):
+        return self.name
