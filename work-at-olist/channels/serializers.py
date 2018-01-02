e62a8886b737be7b3110aeae44d812e82681a02e
Otavio Braga <otaviobraga@MacBook-Air-de-Otavio.local>
2018-01-02 18:38:40 -0200

    Changed serializers to show nested categories and link to channel detail

--- work-at-olist/channels/serializers.py
+++ work-at-olist/channels/serializers.py
@@ -7,20 +7,33 @@ class CategorySerializer(serializers.ModelSerializer):
         This serializer is used to represent a category in JSON
     '''
 
-    parent = serializers.PrimaryKeyRelatedField(
-        read_only=True
-    )
+    parent = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
-        fields = ('name', 'parent', 'created_at', 'channel')
+        fields = ('name', 'parent', 'uuid')
+
+    def get_parent(self, category):
+        if category.parent is not None:
+            return CategorySerializer(category.parent).data
+        else:
+            return None
 
 
 class ChannelSerializer(serializers.ModelSerializer):
     '''
-        This serializer is used to represent a channel in JSON
+        This serializer is used to represent a channel in JSON.
     '''
+
+    link = serializers.HyperlinkedIdentityField(
+        read_only=True,
+        view_name='channels:channel-detail',
+        source='uuid',
+        lookup_field='uuid'
+    )
+
+    category_set = CategorySerializer(many=True)
 
     class Meta:
         model = Channel
-        fields = ('name', 'created_at')
+        fields = ('name', 'uuid', 'category_set', 'link')
